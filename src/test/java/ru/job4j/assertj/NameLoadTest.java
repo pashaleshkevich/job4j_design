335894ada9728908d5b79dd558fe1a20a691485c
Pasha Leshkevich <sum.infinite.jest@gmail.com>
2025-11-26 11:02:19 +0300

    4. Утверждения с исключениями [#504886 #506751] - practice

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NameLoadTest {
+
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkParse() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = new String[0];
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .message()
+                .isNotEmpty();
+    }
+
+    @Test
+    void checkValidateArgument() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {" key=value", "key:value", "key2=value2"};
+        String name = "key:value";
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining(name)
+                .hasMessageContaining("symbol");
+    }
+}
