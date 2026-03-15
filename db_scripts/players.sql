41ea2463d0cd7e6fb9f8e8f7d448d2a29f2d114e
Pasha Leshkevich <sum.infinite.jest@gmail.com>
2026-03-15 18:58:43 +0300

    0. Что такое база данных. [#6860] - practice

---
+++ db_scripts/players.sql
@@ -0,0 +1,34 @@
+CREATE TABLE players (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+	birth_date DATE,
+    field_position VARCHAR(50),
+	university VARCHAR(100),
+    height INTEGER,
+    weight INTEGER,
+    strengths TEXT,     
+    weaknesses TEXT     
+);
+INSERT INTO players (
+    name, 
+    birth_date, 
+    field_position,
+	university, 
+    height, 
+    weight, 
+    strengths, 
+    weaknesses
+) VALUES (
+    'Фернандо Мендоса',
+    '2003-10-01',  
+    'Quarterback',
+    196,
+    102,
+    'Indiana Hoosiers',
+    'Сильная рука. Хорошо читает поле.',
+    'Средний атлетизм.'
+);
+UPDATE players SET 
+    strengths = 'Сильная рука. Хорошо читает поле. Отличная защита мяча. Не боится контакта. Готов к НФЛ.',
+    weaknesses = 'Средний атлетизм. Не является настоящим dual-threat QB для НФЛ.'
+DELETE FROM players;
